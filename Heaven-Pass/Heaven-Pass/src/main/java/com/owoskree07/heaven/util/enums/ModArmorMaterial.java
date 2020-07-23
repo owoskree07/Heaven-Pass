3b201bbc11cec13eed6a7f6f929081f57cb15d45
owoskree07 <68707736+owoskree07@users.noreply.github.com>
2020-07-23 13:44:01 -0500

    Add files via upload

---
+++ Heaven-Pass/Heaven-Pass/src/main/java/com/owoskree07/heaven/util/enums/ModArmorMaterial.java
@@ -0,0 +1,85 @@
+package com.owoskree07.heaven.util.enums;
+
+import com.owoskree07.heaven.ExampleMod;
+import com.owoskree07.heaven.init.ModItems;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+
+    SUN(ExampleMod.MOD_ID + ":sun", 45, new int[] {4, 7, 9, 4}, 9,
+            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, () -> { return Ingredient.fromItems(ModItems.SUNBURST_INGOT.get());}, 0),
+    CLOUD(ExampleMod.MOD_ID + ":cloud", 33, new int[] {2, 5, 6, 2}, 15,
+        SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, () -> { return Ingredient.fromItems(ModItems.CLOUD_INGOT.get());}, 0);
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[] {11, 16, 15, 13 };
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final Supplier<Ingredient> repairMaterial;
+    private final float knockbackResistance;
+
+    ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent,
+                     float toughness, Supplier<Ingredient> repairMaterial, float knockbackResistance) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
+        this.knockbackResistance = knockbackResistance;
+    }
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.get();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float func_230304_f_() {
+        return this.knockbackResistance;
+    }
+
+}
