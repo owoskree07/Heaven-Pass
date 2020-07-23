f972c6baf3c50491c97899029b43f40e554f0053
owoskree07 <68707736+owoskree07@users.noreply.github.com>
2020-07-23 13:39:54 -0500

    Add files via upload

---
+++ Heaven-Pass/Heaven-Pass/src/main/java/com/owoskree07/heaven/events/ModClientEvents.java
@@ -0,0 +1,12 @@
+package com.owoskree07.heaven.events;
+
+import com.owoskree07.heaven.ExampleMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ExampleMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ModClientEvents {
+
+
+
+}
