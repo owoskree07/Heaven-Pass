9ad3c385550c4b21f69cd09dca1b6521390e55bd
owoskree07 <68707736+owoskree07@users.noreply.github.com>
2020-07-23 13:35:49 -0500

    Add files via upload

---
+++ Heaven-Pass/Heaven-Pass/src/main/java/com/owoskree07/heaven/ExampleMod.java
@@ -0,0 +1,45 @@
+package com.owoskree07.heaven;
+
+import com.owoskree07.heaven.init.ModBlocks;
+import com.owoskree07.heaven.init.ModItems;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("hm")
+public class ExampleMod
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "hm";
+
+    public ExampleMod() {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        ModBlocks.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+
+    public static final ItemGroup TAB = new ItemGroup("HeavenTab"){
+        @Override
+        public ItemStack createIcon(){
+            return new ItemStack(ModItems.SUN_DROPLET.get());
+        }
+    };
+
+}
